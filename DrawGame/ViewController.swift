ad5ba6413835508cef66cdaf198e33964c63405b
Carlos R. Villanueva Ousset <crvo84@gmail.com>
2017-10-28 20:12:32 -0500

    Initial commit.

---
+++ DrawGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DrawGame
+//
+//  Created by Carlos Villanueva Ousset on 10/28/17.
+//  Copyright © 2017 Carlos Villanueva Ousset. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

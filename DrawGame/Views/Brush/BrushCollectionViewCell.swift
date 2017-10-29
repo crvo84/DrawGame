2c1df07126e08b04e4cc5f76b539f1c9a194749b
Carlos R. Villanueva Ousset <crvo84@gmail.com>
2017-10-29 14:48:24 -0600

    Implement BrushView.

---
+++ DrawGame/Views/Brush/BrushCollectionViewCell.swift
@@ -0,0 +1,119 @@
+//
+//  BrushCollectionViewCell.swift
+//  DrawGame
+//
+//  Created by Carlos Villanueva Ousset on 10/29/17.
+//  Copyright © 2017 Carlos Villanueva Ousset. All rights reserved.
+//
+
+import UIKit
+
+protocol BrushCollectionViewCellDelegate: class {
+    func didPressButton(brushCollectionViewCell: BrushCollectionViewCell)
+}
+
+class BrushCollectionViewCell: UICollectionViewCell {
+    
+    fileprivate struct Default {
+        static let color: UIColor = .black
+        static let width: CGFloat = 5.0
+        static let borderColor: UIColor = .lightGray
+        static let borderWidth: CGFloat = 0.5
+    }
+    
+    weak var delegate: BrushCollectionViewCellDelegate?
+    
+    fileprivate var brushView = UIView()
+    fileprivate var button = UIButton()
+    
+    var width: CGFloat? { didSet { updateWidth() } }
+    var color: UIColor? { didSet { updateColor() } }
+    var borderColor: UIColor? { didSet { updateColor() } }
+    var borderWidth: CGFloat? { didSet { updateColor() } }
+    var colorWithBorder: UIColor? { didSet { updateColor() } }
+    
+    // MARK: - Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        initialSetup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup
+    
+    fileprivate func initialSetup() {
+        isOpaque = false
+        contentView.isOpaque = false
+        backgroundColor = .clear
+        contentView.backgroundColor = .clear
+        
+        // brush view
+        contentView.addSubview(brushView)
+        brushView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.height.equalTo(Default.width)
+            make.width.equalTo(brushView.snp.height)
+        }
+        
+        // button
+        contentView.addSubview(button)
+        button.backgroundColor = .clear
+        button.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        button.addTarget(self, action: #selector(didPressButton), for: .touchUpInside)
+        
+        updateColor()
+    }
+    
+    // MARK: - Update
+    
+    fileprivate func updateWidth() {
+        let width = width ?? Default.width
+        brushView.snp.updateConstraints { make in
+            make.height.equalTo(width)
+        }
+        contentView.layoutIfNeeded()
+        brushView.layer.cornerRadius = brushView.bounds.height / 2
+        brushView.layer.masksToBounds = true
+    }
+    
+    fileprivate func updateColor() {
+        let color = self.color ?? Default.color
+        brushView.backgroundColor = color
+        
+        if let colorWithBorder = colorWithBorder, colorWithBorder == color {
+            let borderWidth = self.borderWidth ?? Default.borderWidth
+            let borderColor = self.borderColor ?? Default.borderColor
+            brushView.layer.borderWidth = borderWidth
+            brushView.layer.borderColor = borderColor
+        } else {
+            brushView.layer.borderWidth = 0.0
+        }
+    }
+    
+    @objc fileprivate func didPressButton() {
+        delegate?.didPressButton(brushCollectionViewCell: self)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ DrawGame/Views/Brush/BrushesView.swift
@@ -0,0 +1,138 @@
+//
+//  BrushesView.swift
+//  DrawGame
+//
+//  Created by Carlos Villanueva Ousset on 10/29/17.
+//  Copyright © 2017 Carlos Villanueva Ousset. All rights reserved.
+//
+
+import UIKit
+
+protocol BrushesViewDataSource: class {
+    func widths(forBrushesView: BrushesView) -> [CGFloat]
+}
+
+protocol BrushesViewDelegate: class {
+    func didSelectWidth(_ width: CGFloat, brushesView: BrushesView)
+}
+
+class BrushesView: UIView {
+
+    fileprivate struct Geometry {
+        static let cellSizeRatio: CGFloat = 1.0
+        static let cellSpacing: CGFloat = 16.0
+        static let horizontalContentInset: CGFloat = 20.0
+        static let verticalContentInset: CGFloat = 8.0
+    }
+    
+    fileprivate struct Colors {
+        static let backgroundColor: UIColor = .white
+        static let defaultColor: UIColor = .black
+    }
+    
+    fileprivate struct ReuseId {
+        static let brushCell = "BrushCollectionViewCell"
+    }
+    
+    weak var dataSource: BrushesViewDataSource?
+    weak var delegate: BrushesViewDelegate?
+    
+    fileprivate let collectionView: UICollectionView!
+    
+    fileprivate var widths: [CGFloat] = []
+    
+    var color: UIColor? { didSet { updateColor() } }
+    
+    // MARK: - Initialize
+    
+    required override init(frame: CGRect) {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
+        collectionView = UICollectionView(frame: CGRect.zero,
+                                          collectionViewLayout: flowLayout)
+        super.init(frame: frame)
+        
+        initialSetup()
+    }
+    
+    convenience init() {
+        self.init(frame: CGRect.zero)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup
+    
+    fileprivate func initialSetup() {
+        addSubview(collectionView)
+        collectionView.backgroundColor = Colors.backgroundColor
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.isPagingEnabled = true
+        collectionView.register(ColorCollectionViewCell.self,
+                                forCellWithReuseIdentifier: ReuseId.widthCell)
+    }
+    
+    // MARK: - Update
+    
+    func reloadData() {
+        let newWidths = dataSource?.widths(forBrushesView: self)
+        guard self.widths != newWidths else { return } // nothing to update
+        
+        self.widths = newWidths
+        collectionView.reloadData()
+    }
+}
+
+extension BrushesView: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return widths.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ReuseId.widthCell,
+                                                      for: indexPath)
+        
+        if let brushCell = cell as? BrushCollectionViewCell {
+            let width = widths[indexPath.row]
+            brushCell.width = width
+            brushCell.color = color ?? Colors.defaultColor
+            brushCell.delegate = self
+        }
+        
+        return cell
+    }
+}
+
+extension BrushesView: UICollectionViewDelegate {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
